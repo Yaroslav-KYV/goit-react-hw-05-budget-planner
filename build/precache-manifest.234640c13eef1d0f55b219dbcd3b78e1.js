36ccd71ccad03dc30a2c1badc65f1b1154932bd6
Yaroslav <kovshun.yv@gmail.com>
2019-12-10 23:53:09 +0200

    add link to netlify in README

---
+++ build/precache-manifest.234640c13eef1d0f55b219dbcd3b78e1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51a14c02029f9af4ad781c850167fb91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51e3dc74dedbb64774dc",
+    "url": "/static/js/2.47dd0535.chunk.js"
+  },
+  {
+    "revision": "7c4be5cf09548ee07b2a",
+    "url": "/static/js/main.583322a8.chunk.js"
+  },
+  {
+    "revision": "6aaa0a82991b87814982",
+    "url": "/static/js/runtime-main.ebcfbea1.js"
+  }
+]);
